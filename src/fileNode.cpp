b1e39ab47e3b94ab6b0239aae47f28c2bc48eba7
Abhishek <ce22btech11001@iith.ac.in>
2025-04-16 01:01:21 +0530

    Cleaned all files from remote

--- src/fileNode.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/fileNode.hpp"
-
-FileNode::FileNode(string name, string content) 
-    : fileName(name), fileContent(content) {}
-
-FileNode::FileNode() : fileName(""), fileContent("") {}
